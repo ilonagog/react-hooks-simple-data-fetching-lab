dbcf633020f4db9b6f2288aee80b273c4784685f
ilona <gogiashvili.ilona@gmail.com>
2023-01-04 13:38:30 -0500

    commit tree bb7dd06fc93771ca72e675f1af8c22f4bb090fe9

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,28 @@
-// create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+
+    const [dogImage, setDogImage] = useState(null)
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+            .then((response) => response.json())
+            .then((data) => {
+                setDogImage(data.message);
+                setIsLoaded(true);
+            });
+    }, []);
+
+    if (!isLoaded) return <p>Loading...</p>;
+
+    // dogImage.map((dog) => dog.name)
+    return (
+        <div className="App">
+
+            {dogImage && <img src={dogImage} alt="A Random Dog" />}
+        </div>
+    );
+}
+
+export default App;
